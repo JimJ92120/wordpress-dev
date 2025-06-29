a9f0a17e4d330f05f9a85e5edb4c588dc3080426
JimJ92120 <jimj.development@gmail.com>
2025-06-29 18:33:28 +0700

    refactor: add composer validation for submodules setup

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,49 +5,49 @@ MODULE_PATH_LIST_TO_BUILD=(
   "wp-content/plugins/plugin-name"
   "wp-content/themes/theme-name"
 )
-MODULE_PATH_LIST_TO_REMOVE=(
-  "wp-content/mu-plugins/mailhog.php"
-)
-BUILD_DIRECTORY=".build"
 
 if [ -z $ENV ]; then
   echo "missing ENV argument"
-  echo "e.g ./build.sh development"
+  echo "e.g ./setup.sh development"
 
   exit
 fi
 
-echo "building project $ENV..."
+echo "setting up project for $ENV..."
 
-#
-composer run setup $ENV
+if [ "development" == $ENV ]; then
+  composer install
 
-#
-echo "building custom modules..."
-
-for MODULE_PATH in ${MODULE_PATH_LIST_TO_BUILD[@]}; do
-  echo "building \"$MODULE_PATH\"";
-
-  if [ -e "$MODULE_PATH/package.json" ]; then
-    npm --prefix $MODULE_PATH --loglevel=error run build
-  fi
-done
-
-if [ "production" == $ENV ]; then
-  echo "preparing build for $ENV..."
+  echo "installing custom modules custom modules..."
 
   for MODULE_PATH in ${MODULE_PATH_LIST_TO_BUILD[@]}; do
-    echo "removing $MODULE_PATH..."
+    echo "building \"$MODULE_PATH\"";
 
-    rm -rf $MODULE_PATH
+    if [ -e "$MODULE_PATH/composer.json" ]; then
+      composer install --working-dir=$MODULE_PATH
+    fi
+
+    if [ -e "$MODULE_PATH/package.json" ]; then
+      npm --prefix $MODULE_PATH install
+    fi
   done
+elif [ "production" == $ENV ]; then
+  composer install --no-dev
 
-  #
-  rm -rf $BUILD_DIRECTORY
-  mkdir $BUILD_DIRECTORY
+  echo "installing custom modules custom modules..."
 
-  cp -r wp-content/mu-plugins wp-content/plugins wp-content/themes $BUILD_DIRECTORY
+  for MODULE_PATH in ${MODULE_PATH_LIST_TO_BUILD[@]}; do
+    echo "building \"$MODULE_PATH\"";
+
+    if [ -e "$MODULE_PATH/composer.json" ]; then
+      composer validate --strict
+      composer install --working-dir=$MODULE_PATH --no-dev
+    fi
+
+    if [ -e "$MODULE_PATH/package.json" ]; then
+      npm --prefix $MODULE_PATH --loglevel=error ci
+    fi
+  done
 fi
 
-#
 echo "done"
