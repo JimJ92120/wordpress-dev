73e15a1eed4f69b9d0fa1fea5f7bcc546ded597e
JimJ92120 <jimj.development@gmail.com>
2025-07-05 21:55:23 +0700

    format: format with phpcs / phpcbf

--- wp-content/mu-plugins/mailhog.php
+++ wp-content/mu-plugins/mailhog.php
@@ -1,36 +1,59 @@
 <?php
-/*
+/**
  * Plugin Name: Mailhog SMTP Server
- * 
  * Description: Connect to Mailhog SMTP Server via Docker containers.
+ * 
+ * @package Mailhog
  */
 
-// add the action
-add_action( 'wp_mail_failed', function ($wp_error) {
-    print_r( $wp_error, true );
-    // exit;
-}, 10, 1);
+add_action(
+    'wp_mail_failed',
+    function ( $wp_error ) {
+        write_log( $wp_error );
+    },
+    10,
+    1
+);
 
-// configure PHPMailer to send through SMTP
-add_action( 'phpmailer_init', function ($phpmailer) {
-    $phpmailer->isSMTP();
+add_action(
+    'phpmailer_init',
+    function ( $phpmailer ) {
+        $phpmailer->isSMTP();
 
-    $phpmailer->SMTPAuth = false;
-    $phpmailer->SMTPSecure = '';
-    $phpmailer->SMTPAutoTLS = false;
+        // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+        $phpmailer->SMTPAuth = false;
+        // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+        $phpmailer->SMTPSecure = '';
+        // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+        $phpmailer->SMTPAutoTLS = false;
 
-    $phpmailer->Host = SMTP_HOST;
-    $phpmailer->Port = SMTP_PORT;
-    $phpmailer->From = SMTP_FROM;
-    $phpmailer->FromName = SMTP_FROM_NAME;
+        // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+        $phpmailer->Host = SMTP_HOST;
+        // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+        $phpmailer->Port = SMTP_PORT;
+        // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+        $phpmailer->From = SMTP_FROM;
+        // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+        $phpmailer->FromName = SMTP_FROM_NAME;
+        // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+        $phpmailer->SMTPDebug = SMTP_DEBUG;
+    }
+);
 
-    $phpmailer->SMTPDebug = SMTP_DEBUG;
-});
+add_filter(
+    'wp_mail_from',
+    function () {
+        return SMTP_FROM;
+    },
+    10,
+    1
+);
 
-add_filter('wp_mail_from', function () {
-    return SMTP_FROM;
-}, 10, 1);
-
-add_filter('wp_mail_from_name', function() {
-    return  SMTP_FROM_NAME;
-}, 10, 1);
+add_filter(
+    'wp_mail_from_name',
+    function () {
+        return SMTP_FROM_NAME;
+    },
+    10,
+    1
+);
